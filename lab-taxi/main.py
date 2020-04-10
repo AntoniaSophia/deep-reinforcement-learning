a09233da8392f4ebed04cd8a8aeb771dbaa4324e
antoniasophia <antonia.reiter@freenet.de>
2020-04-10 15:20:54 +0200

    Correction in order to use Taxi-v3

--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -3,6 +3,11 @@ from monitor import interact
 import gym
 import numpy as np
 
-env = gym.make('Taxi-v2')
-agent = Agent()
+env = gym.make('Taxi-v3')
+agent = Agent(env.nA)
 avg_rewards, best_avg_reward = interact(env, agent)
+#print("Mode = " , agent.mode)
+#print("Epsilon = " , agent.epsilon)
+#print("Gamma = " , agent.gamma)
+#print("Alpha = " , agent.alpha)
+
